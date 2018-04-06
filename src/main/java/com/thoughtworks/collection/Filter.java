a7d474551308ad96f59829a873a8d57ccecee612
915448715@qq.com <915448715@qq.com>
2018-04-06 17:38:15 +0800

    filter

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
@@ -7,25 +8,43 @@ import java.util.Map;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for(Integer num: array){
+            if(num%2 == 0) result.add(num);
+        }
+        return result;
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for(Integer num: array){
+            if(num%3 == 0) result.add(num);
+        }
+        return result;
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for(Integer num: firstList){
+            for(Integer numS: secondList){
+                if(num == numS) result.add(num);
+            }
+        }
+        return result;
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (Integer num : array){
+            if(!result.contains(num)) result.add(num);
+        }
+        return result;
     }
 }
